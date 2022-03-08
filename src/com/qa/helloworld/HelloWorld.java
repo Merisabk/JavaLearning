13f4b81f0058bd1ebfb851bfe465f09db3cf6e4f
Merisabk <80709468+Merisabk@users.noreply.github.com>
2022-03-08 22:22:45 +0000

    made changes

--- src/com/qa/helloworld/HelloWorld.java
+++ src/com/qa/helloworld/HelloWorld.java
@@ -2,16 +2,18 @@ package com.qa.helloworld;
 
 public class HelloWorld {
 	
-	public void printMessage(String message) 
-	{
-	
-		System.out.println(message);
+	public void helloWorld() {
+		String helloWorld = "Hello World!\nThis is more text";
+		System.out.println(helloWorld);
+	}
+	public void printMessage(String messageOne, String messageTwo, int number) {
+		System.out.println("messageOne " + " messageTwo " + 10);
 	}
 	
 	public String concatText(String x, String y) {
 		return x+ " " +y;
 	}
-	
+	 
 
 
 	
--- src/com/qa/helloworld/Runner.java
+++ src/com/qa/helloworld/Runner.java
@@ -4,7 +4,10 @@ public class Runner {
 	
 	public static void main(String[] args) {
 		
+		
 		HelloWorld myObject = new HelloWorld();
+		myObject.helloWorld();
+		myObject.printMessage("Hello World", "This is more text", 10);
 		
 		String result = myObject.concatText("Hello", "World");
 		System.out.println(result);
