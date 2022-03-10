50995f1fecfed1f7a686efd652e158b93da2e551
Merisabk <80709468+Merisabk@users.noreply.github.com>
2022-03-10 21:18:43 +0000

    reviewed constructors

--- src/com/qa/constructors/Person.java
+++ src/com/qa/constructors/Person.java
@@ -1,13 +1,26 @@
 package com.qa.constructors;
 
+	//In Java, a constructor is just a method. It has the same name as the class. 
+	//a constructor is always the same name as the class. 
+	//every class has a default constructor 
+
 public class Person {
 	
+	// we tend to have properties in our class. 
+	// that could of properties of a person would be variables in our class. No values given because it will be different for every person. 
+	// You can give values - if its always going to be the same. 
 	public String name;
 	public int age; 
 	public char gender;
 	
+	// Constructor doesn't have a return type. That doesn't mean void
+	//(void is a return type that means the method doesn't return anything)
+	// As long as they are different we can have as many constructors as we want. 
 	public Person() {}
 	
+	
+	// this. refers to the class itself.
+	//this. means thisclass.name referring to the class level variables. 
 	public Person  (String name, int age, char gender) {
 		this.name = name; 
 		this.age = age;
--- src/com/qa/constructors/Runner.java
+++ src/com/qa/constructors/Runner.java
@@ -1,27 +1,41 @@
 package com.qa.constructors;
 
+
 public class Runner {
 
 	public static void main(String[] args) {
 		
-	Person John = new Person("John", 20, 'M');
-	System.out.println("Name: " + John.name);
-	System.out.println("Age: " + John.age);
-	System.out.println("Gender: " + John.gender);
+	Person john = new Person("John", 20, 'M');
+	System.out.println("Name: " + john.name);
+	System.out.println("Age: " + john.age);
+	System.out.println("Gender: " + john.gender);
 	
 	System.out.println();
 	
-	Person Jess = new Person("Jess", 21, 'F');
-	System.out.println("Name: " + Jess.name);
-	System.out.println("Age: " + Jess.age);
-	System.out.println("Gender: " + Jess.gender);
+	Person jess = new Person("Jess", 21, 'F');
+	System.out.println("Name: " + jess.name);
+	System.out.println("Age: " + jess.age);
+	System.out.println("Gender: " + jess.gender);
 	
 	System.out.println();
 	
+	// This constructor will use default values
 	Person j = new Person(); 
 	System.out.println("Name: " + j.name);
 	System.out.println("Age: " + j.age);
 	System.out.println("Gender: " + j.gender);
+	
+	System.out.println();
+	
+	j.age = 26;
+	System.out.println("Age: " + j.age);
+	
+	j.name = "Joel";
+	System.out.println("Name: " + j.name);
+	
+	j.gender = 'M';
+	System.out.println("Gender: " + j.gender);
+	
 
 
 
