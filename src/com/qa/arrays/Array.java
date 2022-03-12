7aa0fa5b6602212d10d657c1489e9ce515c5c50c
Merisabk <80709468+Merisabk@users.noreply.github.com>
2022-03-12 21:09:19 +0000

    reviewed arrays

--- src/com/qa/arrays/Array.java
+++ src/com/qa/arrays/Array.java
@@ -1,31 +1,37 @@
 package com.qa.arrays;
 
 public class Array {
-	
-	 int[] array = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
-//	array[0] = 10;
-//	array[1] = 20;
-//	array[2] = 30;
-//	array[3] = 40;
-//	array[4] = 50;
-//	array[5] = 60;
-//	array[6] = 70;
-//	array[7] = 80;
-//	array[8] = 90;
-//	array[9] = 100;
+	//Create an array that will hold 10 integer values, populate the array with values, then call and output each element.
+	public void methodOne() {
+		int[] array = new int[10]; 
+		array[0] = 10;
+		array[1] = 20;
+		array[2] = 30;
+		array[3] = 40;
+		array[4] = 50;
+		array[5] = 60;
+		array[6] = 70;
+		array[7] = 80;
+		array[8] = 90;
+		array[9] = 100;
+		
+ //	Create a for loop that populates an integer array with values, outputting them at each iteration.
+	 for (int i = 0; i < array.length; i++) 
+	 System.out.println(array[i]);
 	 
-	 for (int i = 0; i <=. array.length; i++) {
-		 array[6];
+//Then create another loop that iterates through the array, changing the values at each point to equal itself times 10, 
+//outputting them at each iteration.
+	for (int i = 0; i < array.length; i++) {
+		array[i] = 10;
+		 System.out.println(array[i]);	
+	}
+	
 	 }
+	 
 
-	int x = array[8];
-	System.out.println(x);
-	
-	
-	
+	} 
 	
 
-}
-}
+
 
 
--- src/com/qa/arrays/Runner.java
+++ src/com/qa/arrays/Runner.java
@@ -4,18 +4,64 @@ public class Runner {
 
 	public static void main(String[] args) {
 		
-		int[] array = new int[10];
+		Array a = new Array();
+		a.methodOne();
+	}
 		
 		
-//		int[] numArray; 
-//		int[] anotherNumArray = {10, 23, 45, 76, 87};
-//		int[] finalArray = new int[5];
+// --------------Arrays------------------------
+		// 3 ways to make a basic array 
+//		int[] numArray; // creates a blank integer array 
+//		int[] anotherNumArray = {10, 23, 45, 76, 87}; // creates an integer array with data set in it/ 
+		
+		
+		// creates an empty int array thats of a fixed length. 
+		// This array has 5 index positions. 0-4. 
+//		int[] finalArray = new int[5]; 
+//		// how to assign values in an array. 
+//		finalArray[0] = 10;
+//		finalArray[1] = 15;
+//		finalArray[2] = 20;
+//		finalArray[3] = 25;
+//		finalArray[4] = 30;
+
+		// this loop assigns every index the value of 10
+//		for (int i = 0; i < finalArray.length; i++) {
+//			finalArray[i] = 10; 
+//		}
+		
+		// This goes through the loop and prints out every value. 
+//		for (int i = 0; i < finalArray.length; i++) {
+//			System.out.println(finalArray[i]);	
+//		}
+		
+		// For Each Loop 
+		// It goes through the array one value at a time and stores the value in this variable number. 
+		//for every iteration x will be the value at the next position. 
+		// It will always loop from start to finish. 
+//		for(int number : finalArray) {
+//			System.out.println(number);
+//		}
+		
+		// Making a multi-dimensional array 
+//				int [][] multiDimensionalArray;  // array inside an array
+				int[][] anotherMDArray =  {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}; // 
+		
+		// Looping through multi-dimensional arrays
+//		for (int i = 0; i < anotherMDArray.length; i++) {
+//			for (int j = 0; j < anotherMDArray[i].length; j++) {
+//			System.out.print(anotherMDArray[i][j] + " ");
+//			}
+//			System.out.println();
+//			}
+//		}
+		
+		
 
 		
 		
-//	for(int i = 0; i < array.length; i++) {
-//			System.out.println(array[i]);
-//		}
+		
+		
 		
 		
 		
@@ -37,6 +83,6 @@ public class Runner {
 		}
 		
 		
-	}
+	
 
 
