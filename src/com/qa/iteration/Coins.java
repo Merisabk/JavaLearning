6984d999deee689acdb3b9ac1023f750acf2e5ec
Merisabk <80709468+Merisabk@users.noreply.github.com>
2022-03-12 15:38:34 +0000

    worked on some iteration exercises

--- src/com/qa/iteration/Coins.java
+++ src/com/qa/iteration/Coins.java
@@ -1,10 +1,9 @@
 package com.qa.iteration;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Coins {
 	
-
-
-
 	public void coins (double cost, double payment) {
 		double changeGiven = payment - cost;
 		int tenNote = 0;
@@ -43,4 +42,45 @@ public class Coins {
 		System.out.println("One pence coins: " + onep);
 	}
 
+	
+	public void project() {
+		for (int i =100; i <= 200; i++) {
+		System.out.println(i);
+			}
+	
+	}
+		
+	public void project2() 
+	{
+		for (int A = 100; A%2 == 0; A++) {
+			System.out.println("-");
+			
+		if (A != 0) {
+				System.out.println("*");
+				A++; 
+				if (A <= 200) {
+					continue;
+				} else break;			
+			}		
+			}		
+		}
+	// Create a method that can print out the numbers 1-10 10 times each. 
+	public void methodOne() {
+
+		for (int i = 1; i <=10; i++) {
+			System.out.println(i);
+			
+		
+		}
+	}
+	
+	// Create a method to print the numbers 1 to 10 as many times as the value of that number. 
+
 }
+	
+	
+		
+	
+
+
+
--- src/com/qa/iteration/Runner.java
+++ src/com/qa/iteration/Runner.java
@@ -1,11 +1,71 @@
  package com.qa.iteration;
-
+ 
 public class Runner {
 	
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) 
+	
+	{
 		Coins c = new Coins();
-		c.coins(4.58, 20);
-		
+		c.coins(4.58, 20);	
+		c.project();
+		c.project2();
+		c.methodOne();
+	}
+	
+	
+	
 }
-}
+
+ 
+//public class Runner {
+//	
+//	public static void main(String[] args) {
+//// --------------------------------------------- for ()-loop ------------------------------------------------------------------
+//	// first you define your variable- it will be an int and often called i or j. 
+//	//Start at whatever number you want to start at. Normally loop will start at 0. 
+//	// each component separate by semi-colon ; Then what's the condition. How long is this loop going to go for? 
+//	// i < 10 // as soon as the i is 10 or more the loop stops. 
+//	// if we want to include 10 we have to do <= 10 
+//	// the final part of the loop is always i++ 
+//		// i -- loops backward 
+//			// for (int i = 10; i >=0; i--){
+//			// System.out.println(i);
+//		// i++ means increment by 1. 
+////		for (int i = 0; i < 10; i++) {
+////			
+////		}
+//		
+//// ------------------------ while()-loop-----------------------------
+//		// while loop could run forever. much more flexible.
+//		
+//		boolean flag = true;
+//		int num1 =5;
+//		int num2 = 10;
+//		int count = 0;
+//		// while flag == true 
+//		while(flag) {
+//			System.out.println("I'm looping.");
+//			//break; we can use this to get out of the loop
+//			count++;
+//			
+//			if (count >= 10) {
+//				flag = false; // could also use break; here 
+//			}
+//		}
+//		
+//// ------------- do()-while ()loop -----------------
+//		// the code that repeats will always run once before checking the condition. 
+//		// checks the condition after it loops once whereas while loops checks condidtion first. 
+//		do {
+//			System.out.println("I'm looping");
+//			count++;
+//			if (count > 10) {
+//				flag = false;
+//			} 
+//		} while(flag);
+//		}
+// 		
+//		
+//		
+//}
+//}
