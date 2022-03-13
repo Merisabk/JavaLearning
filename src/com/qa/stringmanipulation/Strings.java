8da3e626dc09862f227db907b7195d5ad7193788
Merisabk <80709468+Merisabk@users.noreply.github.com>
2022-03-13 15:15:02 +0000

    ..

---
+++ src/com/qa/stringmanipulation/Strings.java
@@ -0,0 +1,72 @@
+package com.qa.stringmanipulation;
+
+//public class Strings {
+//	
+//	// creating Strings - 2 methods. 
+//	// Method 1 
+//	public void methodOne(){
+////	String myString = "message";
+////	String anotherString = "message";
+////	
+////	if (myString == anotherString) {
+////		System.out.println("Success!");}
+//	
+//	// Method 2 - two different objects. 
+//	String myString = new String("message");
+//	String anotherString = new String("message");
+//	String thirdString = myString;
+//	
+//	// is the value of the strings the same? 
+////	if (myString.equals(anotherString)) {
+////	System.out.println("Success");
+////	}
+//	
+//	// Are these objects the same?
+//	if (myString == thirdString) {
+//	System.out.println("Success");
+//	}
+//	
+//	// Get the length of the String
+//	System.out.println(myString.length());
+//	
+//	// Concatenating Strings 
+//	String fourthString = myString + " " + anotherString;
+//	System.out.println(fourthString);
+//	
+//	String fifthString = myString.concat(" " ).concat(anotherString).concat(" ").concat(thirdString);
+//	System.out.println(fifthString);
+//	
+//	int num = 10;
+//	String sixthString = fourthString + " " + num;
+//	System.out.println(sixthString);
+	
+	// String manipulation methods 
+//	
+//	// Substring - extract strings from within other strings
+//	System.out.println(thirdString.substring(0, 3));
+//	
+//	// change case
+//	System.out.println(thirdString.toUpperCase());
+//	System.out.println(thirdString.toLowerCase());
+//	
+//	// Get the character at a specific index
+//	System.out.println(thirdString.charAt(3));
+//	
+//	// Get the position/index of a character.
+//	// four index of methods 
+//	// by default it will always return the first occurence. 
+//	//if you want to find another 'e' you need to use other methods.
+//	System.out.println(thirdString.indexOf('e', 2));
+//	
+//	// Starts with 
+//	System.out.println(thirdString.startsWith("Mes"));
+//	
+//	// Replace 
+//	System.out.println(thirdString.replace("Mes", "MES"));
+	
+//	
+//	}
+//	}
+	
+
+
---
+++ src/com/qa/stringmanipulation/StringsExercise.java
@@ -0,0 +1,34 @@
+package com.qa.stringmanipulation;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringsExercise {
+	public void methodOne() {
+		// ------------------Exercise----------------------//
+		// Create two Strings where one string has a value of “yesterday it was raining” and the other string with a value of “today it is sunny”
+		//Concatenate both values, capitalise both strings and print out the result.
+		//The result should be: TODAY IT IS SUNNY, YESTERDAY IT WAS RAINING
+		
+	String a = "yesterday it was raining";
+	String b = "today it is sunny";
+	
+	//System.out.println(a.concat(" \n" ).concat(b));
+	String c = b.toUpperCase() + "," + a.toUpperCase();
+	System.out.println(c);
+
+	// -- Now use the substring method to print out: `TODAY IT IS RAINING`
+   // System.out.println(c.indexOf("RAI"));
+	System.out.println(c.substring(0, 12)+ c.substring(35, 42));
+	
+	int stringWords = a.length();
+	System.out.println(stringWords);
+	
+	
+	
+	
+	}
+	
+		
+	
+	
+}
